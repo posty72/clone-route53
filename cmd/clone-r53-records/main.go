5e12fdb7a0383aea430f388251aab14ebea2322f
Josh <josh@plink.co.nz>
2020-10-02 11:46:27 +1300

    finished cloning records

--- cmd/clone-r53-records/main.go
+++ cmd/clone-r53-records/main.go
@@ -11,27 +11,27 @@ import (
 	"github.com/aws/aws-sdk-go/service/route53"
 )
 
-// var name string
-// var target string
-// var TTL int64
 var weight = int64(1)
 var sourceZoneID string
 var destZoneID string
 var destAccountID string
+var mode string
 
 func init() {
-	// flag.StringVar(&name, "d", "", "domain name")
-	// flag.StringVar(&target, "t", "", "target of domain name")
-	flag.StringVar(&sourceZoneID, "f", "", "AWS Zone Id for domain")
-	// flag.Int64Var(&TTL, "ttl", int64(60), "TTL for DNS Cache")
+	flag.StringVar(&sourceZoneID, "s", "", "AWS Zone Id for domain the existing zone")
+	flag.StringVar(&destZoneID, "d", "", "AWS Zone Id for domain the destination zone")
+	flag.StringVar(&destAccountID, "a", "", "AWS Account Id for the destination zone")
+	flag.StringVar(&mode, "m", "UPSERT", "AWS Account Id for the destination zone")
 
 }
 
 func main() {
 	flag.Parse()
 
-	if sourceZoneID == "" {
-		fmt.Println(fmt.Errorf("incomplete arguments: f: %s", sourceZoneID))
+	fmt.Println(mode)
+
+	if sourceZoneID == "" || destZoneID == "" || destAccountID == "" {
+		fmt.Println(fmt.Errorf("incomplete arguments: s: %s, d: %s: a: %s", sourceZoneID, destZoneID, destAccountID))
 		flag.PrintDefaults()
 		return
 	}
@@ -44,8 +44,61 @@ func main() {
 
 	r53 := route53.New(sess)
 
-	records := listCnames(r53)
+	records := listRecords(r53, sourceZoneID)
 
+	arn := fmt.Sprintf(
+		"arn:aws:iam::%v:role/OrganizationAccountAccessRole",
+		destAccountID,
+	)
+	assumedSession, err := session.NewSession(&aws.Config{
+		Credentials: stscreds.NewCredentials(sess, arn),
+	})
+
+	if err != nil {
+		println(err.Error())
+		panic(err)
+	}
+
+	r53Dest := route53.New(assumedSession)
+
+	createRecords(r53Dest, records)
+}
+
+func listRecords(r53 *route53.Route53, zoneID string) *route53.ListResourceRecordSetsOutput {
+	listParams := &route53.ListResourceRecordSetsInput{
+		HostedZoneId: aws.String(zoneID),
+	}
+	records, err := r53.ListResourceRecordSets(listParams)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Printf("Retrived records for %s", zoneID)
+
+	return records
+}
+
+func createRecords(r53 *route53.Route53, records *route53.ListResourceRecordSetsOutput) {
+	params := &route53.ChangeResourceRecordSetsInput{
+		ChangeBatch: &route53.ChangeBatch{
+			Changes: formatRecordsToChanges(records),
+			Comment: aws.String("Inserted new records"),
+		},
+		HostedZoneId: &destZoneID,
+	}
+
+	response, err := r53.ChangeResourceRecordSets(params)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println(response)
+}
+
+func printRecords(records *route53.ListResourceRecordSetsOutput) {
 	for i := 0; i < len(records.ResourceRecordSets); i++ {
 		println(*records.ResourceRecordSets[i].Name)
 		println(*records.ResourceRecordSets[i].Type)
@@ -57,30 +110,29 @@ func main() {
 
 		println("\n")
 	}
-
-	arn := fmt.Sprintf(
-		"arn:aws:iam::%v:role/OrganizationAccountAccessRole",
-		destAccountID,
-	)
-	assumedCreds := stscreds.NewCredentials(sess, arn)
-
-	assumedSession := session.NewSession(&aws.Config{
-		Credentials: *assumedCreds.Get()
-	})
-
-	r53Dest := route53.New(assumedCreds)
-
 }
 
-func listCnames(r53 *route53.Route53) *route53.ListResourceRecordSetsOutput {
-	listParams := &route53.ListResourceRecordSetsInput{
-		HostedZoneId: aws.String(sourceZoneID),
-	}
-	records, err := r53.ListResourceRecordSets(listParams)
+func formatRecordsToChanges(records *route53.ListResourceRecordSetsOutput) []*route53.Change {
+	changes := make([]*route53.Change, len(records.ResourceRecordSets))
 
-	if err != nil {
-		panic(err.Error())
+	for i := 0; i < len(records.ResourceRecordSets); i++ {
+		if *records.ResourceRecordSets[i].Type != "NS" || *records.ResourceRecordSets[i].Name != "ngatirangi.com." {
+			changes[i] = &route53.Change{
+				Action:            aws.String("UPSERT"),
+				ResourceRecordSet: records.ResourceRecordSets[i],
+			}
+		}
 	}
 
-	return records
+	filteredChanges := make([]*route53.Change, 0)
+
+	for _, item := range changes {
+		if item != nil {
+			filteredChanges = append(filteredChanges, item)
+		}
+	}
+
+	fmt.Println(filteredChanges)
+
+	return filteredChanges
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/posty72/clone-route53
+
+go 1.13
+
+require github.com/aws/aws-sdk-go v1.35.1
